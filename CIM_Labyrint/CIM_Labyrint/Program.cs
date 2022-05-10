534fc90063a3a23a9f1a196919ee7e65f65f4eee
DrNapNap <54890080+DrNapNap@users.noreply.github.com>
2022-05-10 12:07:12 +0200

    <xz<X

--- CIM_Labyrint/CIM_Labyrint/Program.cs
+++ CIM_Labyrint/CIM_Labyrint/Program.cs
@@ -7,7 +7,7 @@ namespace CIM_Labyrint
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new GameWorld())
                 game.Run();
         }
     }
