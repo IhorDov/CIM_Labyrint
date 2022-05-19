e6f229a74bd475bf1673c7ac6a059978cac03262
DrNapNap <54890080+DrNapNap@users.noreply.github.com>
2022-05-19 10:55:05 +0200

    alog

---
+++ CIM_Labyrint/CIM_Labyrint/Algoritme/Edge.cs
@@ -0,0 +1,17 @@
+﻿
+namespace CIM_Labyrint
+{
+    class Edge<T>
+    {
+
+        public Node<T> From { get; private set; }
+
+        public Node<T> To { get; private set; }
+
+        public Edge(Node<T> from, Node<T> to)
+        {
+            this.From = from;
+            this.To = to;
+        }
+    }
+}
---
+++ CIM_Labyrint/CIM_Labyrint/Algoritme/StartAlgoritme.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace CIM_Labyrint
+{
+    public class StartAlgoritme
+    {
+        private Thread StartAlgoritmeThread;
+
+        public static Semaphore mySemaphore = new Semaphore(1, 1);
+
+
+        public StartAlgoritme()
+        {
+            StartAlgoritmeThread = new Thread(StartBFS);
+
+            StartAlgoritmeThread.IsBackground = true;
+
+            StartAlgoritmeThread.Start();
+
+        }
+
+        private void StartBFS()
+        {
+            while (true)
+            {
+                BeregningePosition();
+            }
+
+        }
+
+        void BeregningePosition()
+        {
+            mySemaphore.WaitOne();
+
+
+            mySemaphore.Release();
+
+        }
+
+
+        private static Node<T> DFS<T>(Node<T> start, Node<T> goal)
+        {
+            Queue<Edge<T>> edges = new Queue<Edge<T>>();
+            edges.Enqueue(new Edge<T>(start, start));
+            while (edges.Count > 0)
+            {
+                Edge<T> edge = edges.Dequeue();
+
+                if (!edge.To.Discovered)
+                {
+                    edge.To.Discovered = true;
+
+                    edge.To.Parent = edge.From;
+
+                }
+                if (edge.To == goal)
+                {
+
+                    return edge.To;
+                }
+
+                foreach (var item in edge.To.Edges)
+                {
+                    if (!item.To.Discovered)
+                    {
+
+                        Thread.Sleep(30);
+                        edges.Enqueue(item);
+                    }
+                }
+
+
+            }
+            return null;
+        }
+        private static List<Node<T>> FindPath<T>(Node<T> node, Node<T> start)
+        {
+            List<Node<T>> path = new List<Node<T>>();
+            while (!node.Equals(start))
+            {
+                path.Add(node);
+                node = node.Parent;
+            }
+            path.Add(start);
+            path.Reverse();
+            return path;
+        }
+    }
+}
